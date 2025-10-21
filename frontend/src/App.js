027b1045f77eed0eee282149c6847ed226d653a0
Zo Qazi <zozo@Mac.lan>
2025-10-21 15:06:45 -0500

    Removed AWS login to allow open access

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
-import { withAuthenticator } from "@aws-amplify/ui-react";
-import "@aws-amplify/ui-react/styles.css";
+//import { withAuthenticator } from "@aws-amplify/ui-react";
+//import "@aws-amplify/ui-react/styles.css";
 
-import { Amplify, Auth } from "aws-amplify";
+//import { Amplify, Auth } from "aws-amplify";
 //import awsconfig from "./aws-exports";
 import "./App.css";
 import ASLForm from "./components/ASLForm.js";
@@ -35,5 +35,5 @@ function App({ signOut, user }) {
     </div>
   );
 }
-// export default App;
-export default withAuthenticator(App);
+export default App;
+//export default withAuthenticator(App);
