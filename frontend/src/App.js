a2a347cf47977c84dd10a1e51d32ef16f662030c
Zo Qazi <zozo@Mac.lan>
2025-10-21 14:58:56 -0500

    Successful local build: resolved AWS Amplify and dependency issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,12 @@ import { withAuthenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 
 import { Amplify, Auth } from "aws-amplify";
-import awsconfig from "./aws-exports";
+//import awsconfig from "./aws-exports";
 import "./App.css";
 import ASLForm from "./components/ASLForm.js";
 
-Amplify.configure(awsconfig);
-Auth.configure(awsconfig);
+//Amplify.configure(awsconfig);
+//Auth.configure(awsconfig);
 
 function App({ signOut, user }) {
   return (
